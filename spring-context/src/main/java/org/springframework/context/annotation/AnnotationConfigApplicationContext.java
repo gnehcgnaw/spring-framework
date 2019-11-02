e9d155c899edb15111c231546f793463fbbcaa3c
王成 <gnehcgnaw@gamil.com>
2019-11-02 11:49:28 +0800

    为register()方法添加理解下注解。

--- spring-context/src/main/java/org/springframework/context/annotation/AnnotationConfigApplicationContext.java
+++ spring-context/src/main/java/org/springframework/context/annotation/AnnotationConfigApplicationContext.java
@@ -167,10 +167,11 @@ public class AnnotationConfigApplicationContext extends GenericApplicationContex
 
 	/**
 	 * Register one or more component classes to be processed.
+	 * 注册一个或多个要处理的组件类 ,例如：spring-leanring中的${@link red.reksai.dao.UserDao}或者${@link red.reksai.config.AppConfig}
+	 * 在注册之后，必须调用{@link #refresh()} 才能完全解析上下文。
 	 * <p>Note that {@link #refresh()} must be called in order for the context
 	 * to fully process the new classes.
-	 * @param componentClasses one or more component classes &mdash; for example,
-	 * {@link Configuration @Configuration} classes
+	 * @param componentClasses one or more component classes &mdash; for example, {@link Configuration @Configuration} classes ，参数类型是一个可变的数组
 	 * @see #scan(String...)
 	 * @see #refresh()
 	 */
